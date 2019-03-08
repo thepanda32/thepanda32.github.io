00a75cd52efbee2e1aaa795850bc975e32af37ba
Chase <33735766+thepanda32@users.noreply.github.com>
2019-03-07 22:55:46 -0700

    messing with js function getCondition

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -2,3 +2,23 @@
 Weather Site Javascript Functions
 ***********************************/
 console.log('My javascript is being read.');
+
+
+var weatherCondition = document.getElementById("situation-statement").innerHTML;
+
+console.log(weatherCondition) //this is just to test the weatherCondition variable ^
+
+function getCondition(weatherCondition){
+  var switcher = weatherCondition
+  switch (weatherCondition) {
+    case "clear":
+      console.log("The weather is clear")
+
+      break;
+    default:
+    console.log("end of switch");
+
+  }
+}
+
+getCondition()
